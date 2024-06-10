6f042c4853fdb59a9b2b05aaa2751e61d5a9191f
Pablo Silva <pablonicolassilvabravo@gmail.com>
2024-06-09 21:50:14 -0400

    change project model to interface

--- src/models/project.ts
+++ src/models/project.ts
@@ -1,10 +1,10 @@
 import mongoose, { Document, Schema } from "mongoose";
 
-export type ProjectType = Document & {
+export interface IProject extends Document {
   projectName: string;
   clientName: string;
   description: string;
-};
+}
 
 const ProjectSchema: Schema = new Schema({
   projectName: {
@@ -24,5 +24,5 @@ const ProjectSchema: Schema = new Schema({
   },
 });
 
-const Project = mongoose.model<ProjectType>('Project', ProjectSchema)
-export default Project
+const Project = mongoose.model<IProject>("Project", ProjectSchema);
+export default Project;
