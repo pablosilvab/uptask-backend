60deec4410fb7fa52da843be26babd7d745573d8
Pablo Silva <pablonicolassilvabravo@gmail.com>
2024-06-10 18:44:09 -0400

    enable cors

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ UpTask is a simple application designed to help you manage your tasks, similar t
 For local development, use a `.env` file and add the following variables:
 
 * DATABASE_URL= mongodb+srv://user:pass@host/dbname?retryWrites=true&w=majority&appName=Cluster0/
+* FRONTEND_URL for the whitelist
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
-import projectRoutes from "./routes/projectRoutes"
+import projectRoutes from "./routes/projectRoutes";
+import { corsConfig } from "./config/cors";
+import cors from "cors";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-app.use(express.json())
-app.use('/api/projects', projectRoutes)
+app.use(cors(corsConfig));
+app.use(express.json());
+app.use("/api/projects", projectRoutes);
 
 export default app;
