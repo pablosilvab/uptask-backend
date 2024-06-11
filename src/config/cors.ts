98d0d0bce7b306d1c4296006b0d0e32714897195
Pablo Silva <pablonicolassilvabravo@gmail.com>
2024-06-10 20:46:42 -0400

    fix local development

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,6 +2,9 @@ import { CorsOptions } from "cors";
 
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
+    if (!origin) {
+      return callback(null, true);
+    }
     const whiteList = [process.env.FRONTEND_URL];
     if (whiteList.includes(origin)) {
       callback(null, true);
